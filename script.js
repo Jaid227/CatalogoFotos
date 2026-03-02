69b7b81ab2993ca56f6a7475f474a419a7f271cd
Jaid Lozano <jaidlozanoreyes@gmail.com>
2026-03-02 00:27:05 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function mostrarSeccion(id){
+    document.querySelectorAll('.seccion').forEach(sec=>{
+        sec.classList.remove('activa');
+    });
+    document.getElementById(id).classList.add('activa');
+}
+
+function abrirImagen(img){
+    const visor = document.getElementById('visor');
+    const imagenGrande = document.getElementById('imagenGrande');
+    imagenGrande.src = img.src;
+    visor.style.display = "flex";
+}
+
+function cerrarImagen(){
+    document.getElementById('visor').style.display = "none";
+}
